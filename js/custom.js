fa657b09eb50eb937eda393f4ddfe43c617a8a45
worthliu <1031559976@qq.com>
2018-11-15 10:58:02 +0800

    website home page

--- js/custom.js
+++ js/custom.js
@@ -8,6 +8,35 @@ $(window).load(function(){
 
 // HOME BACKGROUND SLIDESHOW
 $(function(){
+
+	var bodyEl = document.body,
+		openbtn = document.getElementById( 'open-button' ),
+		closebtn = document.getElementById( 'close-button' ),
+		isOpen = false;
+	//
+	function initEvents() {
+		openbtn.addEventListener( 'click', toggleMenu );
+		if( closebtn ) {
+			closebtn.addEventListener( 'click', toggleMenu );
+		}
+	}
+	//
+	function toggleMenu() {
+		if( isOpen ) {
+			classie.remove( bodyEl, 'show-menu' );
+		}
+		else {
+			classie.add( bodyEl, 'show-menu' );
+		}
+		isOpen = !isOpen;
+	}
+
+	function init() {
+		initEvents();
+	}
+	//
+	init();
+	//
     jQuery(document).ready(function() {
 		$('body').backstretch([
 	 		 "images/bg-slide-1.jpg", 
@@ -16,3 +45,5 @@ $(function(){
 	 			], 	{duration: 3200, fade: 1300});
 		});
 })
+
+
